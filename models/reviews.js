64ce0d5161ca6b171402337ec1f0af0eb66e0e8d
Vishal Dhanda <vishalsinghdhanda@hotmail.com>
2024-10-09 18:42:57 +0100

    Main branch

---
+++ models/reviews.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+
+const reviewSchema = new schema({
+    body: String,
+    rating:{ 
+        type: Number,
+        }
+})    
+
+module.exports = mongoose.model('Review', reviewSchema)
+
