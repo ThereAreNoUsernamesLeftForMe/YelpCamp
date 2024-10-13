febf1860119dce10f0bed64b8943289c2f5e76a5
Vishal Dhanda <vishalsinghdhanda@hotmail.com>
2024-10-13 19:25:24 +0100

    Test

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const validateCampground = (req,res,next) => {
 const validateReview = (req, res, next) => {
     const {error} = reviewSchema.validate(req.body)
     // console.log(error) 
-    console.log(req.body)  
+    // console.log(req.body)  
     if(error){
         const msg = error.details.map(element => element.message).join(',')
         throw new ExpressError(msg, 400)
